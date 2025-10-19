dcbb6186e44b1f08e50c0e67d7d6beb76031b85c
Pushkar Kumar Purve <kpushkar658@gmail.com>
2025-10-19 13:26:00 +0530

    add ability to create new tasks

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector('.menu');
 const open = document.querySelector('.open');
 const close = document.querySelector('.close');
+const addTaskSection = document.getElementById('add-task');
+console.log(addTaskSection);
 
 open.addEventListener('click',()=>{
     menu.classList.remove('-top-50');
@@ -15,3 +17,36 @@ close.addEventListener('click', ()=>{
     open.classList.remove('hidden');
     close.classList.add('hidden');
 })
+
+
+
+function showAddTask(){
+    addTaskSection.classList.remove('hidden');
+}
+function hideAddTask(){
+    addTaskSection.classList.add('hidden')
+}
+function addTask(){
+    addTaskSection.classList.add('hidden');
+    const taskName = document.getElementById('taskName').value.trim();
+    let cato = document.getElementById('taskCato').value.trim();
+    document.getElementById('taskName').value = ''
+    document.getElementById('taskCato').value = ''
+    console.log(taskName);
+    console.log(cato);
+    const taskList = document.getElementById('task-list')
+    if (taskName){
+        if (!cato){
+            cato = 'Nil'
+        }
+        newTask = `<li class="flex justify-between">
+                            <span>${taskName}</span>
+                            <span>${cato}</span>
+                            <span>pending</span>
+                        </li>
+                    `
+        taskList.insertAdjacentHTML('afterend', newTask);
+
+    }
+    
+}
